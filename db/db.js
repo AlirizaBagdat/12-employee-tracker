e4b1d5aab8655fa575e6f4182b3f13f469cc74c2
Aliriza Bagdat <ambagdat@gmail.com>
2023-12-21 23:26:11 +0000

    functions of actions

--- db/db.js
+++ db/db.js
@@ -7,3 +7,33 @@ const connection = mysql.createConnection({
   password: 'your_password',
   database: 'your_database',
 });
+
+async function executeQuery(query, params) {
+    const [rows] = await connection.execute(query, params);
+    return rows;
+  }
+  
+  async function viewDepartments() {
+    const departments = await executeQuery(viewDepartmentsQuery);
+    console.table(departments);
+  }
+  
+  async function viewRoles() {
+    const roles = await executeQuery(viewRolesQuery);
+    console.table(roles);
+  }
+  
+  async function viewEmployees() {
+    const employees = await executeQuery(viewEmployeesQuery);
+    console.table(employees);
+  }
+  
+  async function addDepartment() {
+    const departmentName = await inquirer.prompt({ type: 'input', name: 'name', message: 'Enter the name of the department:' });
+    await executeQuery(addDepartmentQuery, [departmentName.name]);
+    console.log('Department added successfully!');
+  }
+  
+  // Similar functions for addRole, addEmployee, and updateEmployeeRole
+  
+  module.exports = { viewDepartments, viewRoles, viewEmployees, addDepartment, /* addRole, addEmployee, updateEmployeeRole */ };
