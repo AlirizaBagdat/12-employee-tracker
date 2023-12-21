f254f756196f3afd19544f4680eddee5ca32dafe
Aliriza Bagdat <ambagdat@gmail.com>
2023-12-21 23:16:05 +0000

    Values comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,7 @@ INSERT INTO department (title)
 
 VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
+-- Values for roles
 INSERT INTO roles (title, salary, department_id)
 VALUES ('Sales Lead', 100000, 4),
 ('Salesperson', 80000, 4),
@@ -12,13 +13,14 @@ VALUES ('Sales Lead', 100000, 4),
 ('Legal Team Lead', 250000, 3),
 ('Lawyer', 190000, 3);
 
+--Values for employees
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL),
-       ('Mike', 'Chan', 2, 1),
-       ('Ashley', 'Rodriguez', 3, NULL),
-       ('Kevin', 'Tupik', 4, 3),
-       ('Kunal', 'Singh', 5, NULL),
-       ('Malia', 'Brown', 6, 5),
-       ('Sarah', 'Lourd', 7, NULL),
-       ('Tom', 'Allen', 8, 7);
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, NULL),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, NULL),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, NULL),
+('Tom', 'Allen', 8, 7);
 
