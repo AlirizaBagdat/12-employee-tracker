f72a4cec4ca2d095459b6fe36a04703d1a4705bf
Aliriza Bagdat <ambagdat@gmail.com>
2023-12-21 23:25:37 +0000

    create sql connection

---
+++ db/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+const { viewDepartmentsQuery, viewRolesQuery, viewEmployeesQuery, addDepartmentQuery, addRoleQuery, addEmployeeQuery, updateEmployeeRoleQuery } = require('./queries');
+
+const connection = mysql.createConnection({
+  host: 'your_host',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+});
